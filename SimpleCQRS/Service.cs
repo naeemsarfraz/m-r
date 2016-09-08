233bdf8bdf4e39806dea64a4947252b51aee47cc
Naeem Sarfraz <naeem.sarfraz@gmail.com>
2016-09-08 03:06:35 +0100

    a skeleton service

---
+++ SimpleCQRS/Service.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCQRS
+{
+    public class InventoryItemService
+    {
+        private ReadModelFacade _readmodel;
+        private readonly IRepository<InventoryItem> _repository;
+
+        public InventoryItemService(ReadModelFacade readmodel, IRepository<InventoryItem> repository)
+        {
+            _readmodel = readmodel;
+            _repository = repository;
+        }
+    }
+}
